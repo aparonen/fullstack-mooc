cd5a531c1a11170f765742ee2658dd5c318181fe
Anton Paronen <anton.lauri.paronen@gmail.com>
2019-04-11 14:29:38 +0300

    Put parts into array

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -11,44 +11,41 @@ const Part = (props) => (
 
 const Content = (props) => (
     <>
-        <Part part={props.part1} />
-        <Part part={props.part2} />
-        <Part part={props.part3} />
+        <Part part={props.parts[0]} />
+        <Part part={props.parts[1]} />
+        <Part part={props.parts[2]} />
     </>
 )
 
-const Total = (props) => (
-    <p>yhteensä {props.ex1 + props.ex2 + props.ex3} tehtävää</p>
-)
+const Total = (props) => {
+    const total = props.parts.reduce((acc, part) => acc + part.exercises, 0)
+    return (
+        <p>yhteensä {total} tehtävää</p>
+    )
+}
 
 const App = () => {
     const course = 'Half Stack -sovelluskehitys'
-    const part1 = {
-        name: 'Reactin perusteet',
-        exercises: 10,
-    }
-    const part2 = {
-        name: 'Tiedonvälitys propseilla',
-        exercises: 7,
-    }
-    const part3 = {
-        name: 'Komponenttien tila',
-        exercises: 14,
-    }
+    const parts = [
+        {
+            name: 'Reactin perusteet',
+            exercises: 10,
+        },
+        {
+            name: 'Tiedonvälitys propseilla',
+            exercises: 7,
+        },
+        {
+            name: 'Komponenttien tila',
+            exercises: 14,
+        },
+    ]
 
     return (
         <div>
             <Header course={course} />
-            <Content
-                part1={part1}
-                part2={part2}
-                part3={part3}
-            />
-            <Total
-                ex1={part1.exercises}
-                ex2={part2.exercises}
-                ex3={part3.exercises}
-            />
+            <Content parts={parts} />
+            <Total parts={parts} />
         </div>
     )
 }
