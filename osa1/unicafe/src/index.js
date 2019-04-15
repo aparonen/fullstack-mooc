6ce3896fe38d99acfa8d391046771b9583c8eaea
Anton Paronen <anton.lauri.paronen@gmail.com>
2019-04-15 11:35:20 +0300

    Implement feedback app

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,13 +1,49 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
+const Feedback = ({handlers}) => (
+    <div>
+        <h1>Anna palautetta</h1>
+        <button onClick={handlers.good}>Hyvä</button>
+        <button onClick={handlers.neutral}>Neutraali</button>
+        <button onClick={handlers.bad}>Huono</button>
+    </div>
+)
+
+const Statistics = ({reviews}) => (
+    <div>
+        <h1>Statistiikka</h1>
+        <p>Hyvä: {reviews.good}</p>
+        <p>Neutraali: {reviews.neutral}</p>
+        <p>Huono: {reviews.bad}</p>
+    </div>
+)
+
 const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
 
+    const handleGood = () => setGood(good + 1)
+    const handleNeutral = () => setNeutral(neutral + 1)
+    const handleBad = () => setBad(bad + 1)
+
+    const reviews = {
+        good: good,
+        neutral: neutral,
+        bad: bad,
+    }
+    const handlers = {
+        good: handleGood,
+        neutral: handleNeutral,
+        bad: handleBad,
+    }
+
     return (
-        <div>Hello, world!</div>
+        <div>
+            <Feedback handlers={handlers} />
+            <Statistics reviews={reviews} />
+        </div>
     )
 }
 
